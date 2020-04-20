f49ae65531f37f75a96b4276f409990f7e916093
stijn-kamp <stijnkamp@hotmail.com>
2020-04-20 13:19:27 +0200

    Updated stylesheet and script to match bloeddruksymtomen

--- script/escaperoom.vivium.js
+++ script/escaperoom.vivium.js
@@ -1,1 +1,26 @@
-reoufherufh
+const achtergronden = [
+    '#00257A',
+    '#009B74'
+]
+
+function bloeddrukSymtomsClicked(btnId){
+    var btn = document.getElementById(btnId);
+    var btnConfirm = document.getElementById('btnConfirm');
+
+    btn.value++
+    btn.value = btn.value  < achtergronden.length ? btn.value : 0;
+    btn.style.backgroundColor = achtergronden[btn.value]
+    btn.style.color = '#ffffff'
+
+    if(btn.getAttribute('clicked') == null)
+    {
+        btnConfirm.value++
+        if(btnConfirm.value >= 9) btnConfirm.disabled = false;
+        btn.setAttribute('clicked', true)
+    }
+}
+
+function confirmationButtonClicked(btnId) 
+{
+    window.location.href='./bloeddrukToelichting.html'
+}
